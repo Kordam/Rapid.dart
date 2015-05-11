9046887735a021d613233e5ffaf298e742b2e0c8
grizet_j <jean.grizet@epitech.eu>
2015-05-11 13:35:30 -0400

    Test suite todo for kd_tree

---
+++ test/kd_tree_test.dart
@@ -0,0 +1,32 @@
+library rapid.test.kd_tree_test;
+
+import 'package:test/test.dart';
+import 'package:rapid/rapid.dart';
+
+void testBuild()
+{}
+
+void testBuildFromList()
+{}
+
+void testInsert()
+{}
+
+void testRemove()
+{}
+
+void testSearchNear() {}
+
+void testSearchMultiNear() {}
+
+void main()
+{
+  group('KdTree', () {
+    test('Build', testBuild);
+    test('Build from list', testBuildFromList);
+    test('Insert', testInsert);
+    test('Remove', testRemove);
+    test('Search nearest', testSearchNear);
+    test('Search multiple nearest', testSearchMultiNear);
+  });
+}
