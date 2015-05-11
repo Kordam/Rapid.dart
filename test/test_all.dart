42d3729c57bbdf9cfba1cdaaa5673517cb20944c
grizet_j <jean.grizet@epitech.eu>
2015-05-11 14:33:27 -0400

    Update test classes

--- test/test_all.dart
+++ test/test_all.dart
@@ -1,8 +1,10 @@
 library rapid.test.everything;
 
 import 'kd_tree_test.dart' as kdtree;
+import 'world_test.dart' as world;
 
 void main()
 {
   kdtree.main();
+  world.main();
 }
---
+++ test/world_test.dart
@@ -0,0 +1,17 @@
+library rapid.test.world;
+
+import 'package:test/test.dart';
+import 'package:rapid/rapid.dart';
+
+void testBuild()
+{
+
+}
+
+void main()
+{
+  group('World', ()
+  {
+    test('Build', testBuild);
+  });
+}
