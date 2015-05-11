54518f4377a433b9df5bd1d7055444c52a142b98
grizet_j <jean.grizet@epitech.eu>
2015-05-11 14:38:36 -0400

    Add new empty test files for collider and obb_tree

---
+++ test/collider_test.dart
@@ -0,0 +1,13 @@
+library rapid.test.collider_test;
+
+import 'package:test/test.dart';
+import 'package:rapid/rapid.dart';
+
+void testBuild() {}
+
+void main()
+{
+  group('Collider', () {
+    test('Build', testBuild);
+  });
+}
---
+++ test/obb_tree_test.dart
@@ -0,0 +1,22 @@
+library rapid.test.obb_tree;
+
+import 'package:test/test.dart';
+import 'package:rapid/rapid.dart';
+
+void testBuild()
+{
+
+}
+
+void testInsert()
+{
+
+}
+
+void main()
+{
+  group('Obb Tree', () {
+    test('Build', testBuild);
+    test('Insert', testInsert);
+  });
+}
