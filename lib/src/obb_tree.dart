026976e4d49c74b27613dcfde58a71406b958df7
grizet_j <jean.grizet@epitech.eu>
2015-05-21 03:18:42 -0400

    Top-Down points recursive construction function is _split

--- lib/src/obb_tree.dart
+++ lib/src/obb_tree.dart
@@ -23,10 +23,13 @@ class ObbTree
   //Construct an ObbTree
   ObbTree.fromPoints(List<Vector3> points, int divide)
   {
-    Obb3 box = Obb3_fitFromPoints(points);
+    Vector m = new Vector3.zero();
+    Obb3 box = Obb3_fitFromPoints(points, mean: m);
+    //Allocate Left part
+    Vector3 left_mean = new Vector3.zero();
     _root = new ObbTreeNode(1, box, points: points, leaf: true);
-    if (divide > 1) {
-      _split(_root, 1, divide);
+    if (divide >= 1) {
+      _split(_root, m, 1, divide);
       _root.leaf = false;
       _root.points.clear(); //Clear points after subdivision
       _root.points = null;
@@ -34,20 +37,44 @@ class ObbTree
   }
 
   //Recursive method that splits the [parent] node
+  //on the longest axis of [node] and the point [mean]
   //by filling the left an right child nodes
-  void _split(ObbTreeNode parent, int currentDepth, int maxDepth)
+  void _split(ObbTreeNode parent, Vector3 mean, int currentDepth, int maxDepth)
   {
+
+    //NOT THAT SIMPLE
+    var p = Plane_fromAxisAndPoint(Obb3_longestAxis(parent.box), mean);
+
+    //Sort points
+    List<Vector3> left_points = new List<Vector3>();
+    List<Vector3> right_points  = new List<Vector3>();
+
+    //Allocate Left part
+    Vector3 left_mean = new Vector3.zero();
+    Obb3 left_box = Obb3_fitFromPoints(left_points, mean: left_mean);
+    ObbTreeNode left = new ObbTreeNode(currentDepth, left_box, points: left_points);
+
+    //Allocate right part
+    Vector3 right_mean = new Vector3.zero();
+    Obb3 right_box = Obb3_fitFromPoints(right_points, mean: right_mean);
+    ObbTreeNode right = new ObbTreeNode(currentDepth, right_box, points: right_points);
+
+
+    //Assign parent node left & right
+    parent.left = left;
+    parent.right = right;
+
     currentDepth++;
-    if (currentDepth == maxDepth) {
+    if (currentDepth >= maxDepth) {
       parent.left.leaf = true;
       parent.right.leaf = true;
       return;
     }
     else {
-      _split(parent.left, currentDepth, maxDepth);
+      _split(parent.left, left_mean, currentDepth, maxDepth);
       parent.left.points.clear(); //Clear points after subdivision
       parent.left.points = null;
-      _split(parent.right, currentDepth, maxDepth);
+      _split(parent.right, right_mean, currentDepth, maxDepth);
       parent.right.points.clear(); //Clear points after subdivision
       parent.right.points = null;
     }
