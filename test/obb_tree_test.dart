a3c645a41d5a921927a452afc6df6b3a41c9939e
grizet_j <jean.grizet@epitech.eu>
2015-05-23 04:43:03 -0400

    Tests for Obb recursive construction

--- test/obb_tree_test.dart
+++ test/obb_tree_test.dart
@@ -26,6 +26,28 @@ void testBuild()
   expect(box.center.z, isNot(0.0));
 }
 
+void testBuildSplit()
+{
+  var points = new List<Vector3>();
+  points.add(new Vector3(1.0, 5.0, 1.0));
+  points.add(new Vector3(10.0, 5.0, 12.0));
+  points.add(new Vector3(15.0, 5.0, 1.0));
+  points.add(new Vector3(20.0, 5.0, 0.0));
+  points.add(new Vector3(-1.0, 5.0, 100.0));
+  points.add(new Vector3(10.0, 8.0, 56.0));
+  points.add(new Vector3(45.0, 87.0, -1.0));
+  points.add(new Vector3(20.0, 7.0, 0.0));
+  points.add(new Vector3(42.0, 5.0, -3.0));
+  points.add(new Vector3(42.0, 56.0, 0.0));
+  points.add(new Vector3(55.0, 5.0, 1.0));
+  points.add(new Vector3(25.0, 125.0, -50.0));
+  var tree = new ObbTree.fromPoints(points, 2);
+  var box = tree.rootBox;
+  expect(box.center.x, isNot(0.0));
+  expect(box.center.y, isNot(0.0));
+  expect(box.center.z, isNot(0.0));
+}
+
 void testCube()
 {
   var points = new List<Vector3>();
@@ -60,7 +82,7 @@ void testCubeDivide_1()
   points.add(new Vector3(10.0, 0.0, 10.0)); //Right face bl
   points.add(new Vector3(10.0, 10.0, 10.0)); //Right face tl
 
-  var tree = new ObbTree.fromPoints(points, 1);
+  var tree = new ObbTree.fromPoints(points, 2);
   var box = tree.rootBox;
   expect(box.center.x, equals(5.0));
   expect(box.center.y, equals(5.0));
@@ -74,7 +96,8 @@ void main()
 {
   group('Obb Tree', () {
     test('Build random no divide', testBuild);
+    test('Build random 2 nodes', testBuildSplit);
     test('Cube no divide', testCube);
-    test('Cube with divide 1', testCubeDivide_1);
+    test('Cube with 2 nodes', testCubeDivide_1);
   });
 }
