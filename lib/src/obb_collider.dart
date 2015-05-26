851879d6ceea65efced917cdee03fca3900f0ef9
grizet_j <jean.grizet@epitech.eu>
2015-05-25 22:55:37 -0400

    Obb_collider interface has a collidewith(other), should check collison and return colliding geometry

--- lib/src/obb_collider.dart
+++ lib/src/obb_collider.dart
@@ -1,6 +1,6 @@
 part of rapid;
 
-//Define a collider using an oriented bounding box
+//Define a collider using an oriented bounding box tree
 class ObbCollider
 {
   ObbTree        _tree;
@@ -17,5 +17,12 @@ class ObbCollider
   ObbCollider.fromTriangles(List<int> tris, List<Vector3> vertices, {split: 4})
   {
     _tree = new ObbTree.fromTriangles(tris, vertices, 4);
+  }
+
+  //Check if current ObbCollider collide with another ObbCollider,
+  //If so, Lists of colliding [points] and faces [idx] can be filled
+  bool collideWith(ObbCollider oth, {List<int> idx: null, List<Vector3> points: null})
+  {
+    return false;
   }
 }
