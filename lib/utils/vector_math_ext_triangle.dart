741717b302a07c8d3bc7d14aec9c16c822859ffe
grizet_j <jean.grizet@epitech.eu>
2015-06-08 18:48:31 -0400

    Test triangle-triangle yeahhhh

--- lib/utils/vector_math_ext_triangle.dart
+++ lib/utils/vector_math_ext_triangle.dart
@@ -1,16 +1,16 @@
 part of rapid;
 
-
 Plane Triangle_toPlane(Triangle that)
 {
-  var v1 = that.point0 - that.point1;
-  var v2 = that.point0 - that.point2;
+  var v1 = that.point1 - that.point0;
+  var v2 = that.point2 - that.point0;
   var normal = v1.cross(v2);
-  var d = normal.dot(that.point0); //normal[0] * that.point0[0] + normal[1] * that.point0[1] + normal[2] * that.point0[2];
+  normal *= 1.0 / sqrt(normal.x * normal.x + normal.y * normal.y + normal.z * normal.z);
+  var d = - (normal[0] * that.point0[0] + normal[1] * that.point0[1] + normal[2] * that.point0[2]);
   return new Plane.normalconstant(normal, d);
 }
 
-bool Triangle_intersectWithTriangleBasic(Triangle t1, Triangle t2) {
+bool Triangle_intersectsWithTriangleBasic(Triangle t1, Triangle t2) {
 
   //Checking distance to planes
   Plane p1 = Triangle_toPlane(t1);
@@ -23,23 +23,28 @@ bool Triangle_intersectWithTriangleBasic(Triangle t1, Triangle t2) {
   double d5 = p1.distanceToVector3(t2.point2);
   //Trivial reject if all points of t1 are on the same side of t2
   if ((d0 > 0.0 && d1 > 0.0 && d2 > 0.0) || (d0 < 0.0 && d1 < 0.0 && d2 < 0.0)){
-    print("p2 false"); return false;
+    return false;
   }
+
   //Trivial reject if all points of t2 are on the same side of t1
   if ((d3 > 0.0 && d4 > 0.0 && d5 > 0.0) || (d3 < 0.0 && d4 < 0.0 && d5 < 0.0)){
-    print("p1 false"); return false;
+    return false;
   }
 
   //Test t1 edges intersection with t2
   List rays = new List();
+  bool inter = false;
   rays.add(new Ray.originDirection(t1.point0, t1.point1 - t1.point0));
   rays.add(new Ray.originDirection(t1.point1, t1.point2 - t1.point1));
   rays.add(new Ray.originDirection(t1.point2, t1.point0 - t1.point2));
   rays.forEach((Ray r) {
     double d = r.intersectsWithTriangle(t2);
-    if (d != null && d > 0.0 && d < 1.0)
-      return true;
+    if (d != null && d > 0.0 && d < 1.0) {
+      inter = true;
+    }
   });
+  if (inter == true)
+    return true;
 
   //Test t2 edges intersection with t1
   rays.clear();
@@ -47,14 +52,13 @@ bool Triangle_intersectWithTriangleBasic(Triangle t1, Triangle t2) {
   rays.add(new Ray.originDirection(t2.point1, t2.point2 - t2.point1));
   rays.add(new Ray.originDirection(t2.point2, t2.point0 - t2.point2));
   rays.forEach((Ray r) {
-    double d = r.intersectsWithTriangle(t2);
-    if (d != null && d > 0.0 && d < 1.0)
-      return true;
+    double d = r.intersectsWithTriangle(t1);
+    if (d != null && d > -1.0 && d < 1.0)
+      inter = true;
   });
 
-  return false;
+  return inter;
 }
-
 /*
   print("Distance p2[${p2.normal}][${p2.constant}] t1 point0[${t1.point0.toString()}] == ${d0}");
   print("Distance p2[${p2.normal}][${p2.constant}] t1 point1[${t1.point1.toString()}] == ${d1}");
@@ -73,7 +77,6 @@ bool Triangle_intersectsWithTriangle(Triangle that, Triangle other)
   double d2 = p2.distanceToVector3(that.point2);
   //Trivial reject if all points of that are on the same side of other
   if ((d0 > 0.0 && d1 > 0.0 && d2 > 0.0) || (d0 < 0.0 && d1 < 0.0 && d2 < 0.0)){
-    print("p2 false");
     return false;
   }
 
@@ -83,34 +86,27 @@ bool Triangle_intersectsWithTriangle(Triangle that, Triangle other)
   double d5 = p1.distanceToVector3(other.point2);
   //Trivial reject if all points of other are on the same side of that
   if ((d3 > 0.0 && d4 > 0.0 && d5 > 0.0) || (d3 < 0.0 && d4 < 0.0 && d5 < 0.0)){
-    print("p1 false");
     return false;
   }
 
   //Compute Intersection Line of p1 and p2
   Vector3 line_D = p1.normal.cross(p2.normal);
-  print("Line D: ${line_D.toString()}");
-
   //Compute t1 points projection on line distance
   double proj_0 = line_D.dot(that.point0);
   double proj_1 = line_D.dot(that.point1);
   double proj_2 = line_D.dot(that.point2);
-  print("t1 projection on line [${proj_0}][${proj_1}][${proj_2}]");
-
   //Compute t2 points projection on line distance
   double proj_3 = line_D.dot(other.point0);
   double proj_4 = line_D.dot(other.point1);
   double proj_5 = line_D.dot(other.point2);
-  print("t2 projection on line [${proj_3}][${proj_4}][${proj_5}]");
 
   //Find that intervals with line
   double t1 = proj_0 + (proj_1 - proj_0) * (d0 / (d0 - d1));
   double t2 = proj_0 + (proj_2 - proj_0) * (d0 / (d0 - d2));
-  print("T1 intervalues = [${t1}][${t2}]");
+
   //Find other intervals with line
   double s1 = proj_3 + (proj_4 - proj_3) * (d3 / (d3 - d4));
   double s2 = proj_4 + (proj_5 - proj_4) * (d4 / (d4 - d5));
-  print("T2 intervalues = [${s1}][${s2}]");
 
   //Return intervals overlap test
   //Source: GPGPU Programming for Games and Science By David H. Eberly
--- test/utils.dart
+++ test/utils.dart
@@ -7,13 +7,13 @@ import 'package:vector_math/vector_math.dart';
 void trianglesIntersectionFail1() {
   Triangle tri1 = new Triangle.points(new Vector3(0.0, 0.0, 0.0), new Vector3(0.0, 1.0, 1.0), new Vector3(0.0, 0.5, 2.0));
   Triangle tri2 = new Triangle.points(new Vector3(1.0, 0.0, 0.0),  new Vector3(3.0, 1.0, 0.0), new Vector3(5.0, 0.5, 0.0));
-  expect(Triangle_intersectsWithTriangle(tri1, tri2), false);
+  expect(Triangle_intersectsWithTriangleBasic(tri1, tri2), false);
 }
 
 void trianglesIntersectionFail2() {
   Triangle tri1 = new Triangle.points(new Vector3(0.0, 0.0, 0.0), new Vector3(0.0, 1.0, 0.0), new Vector3(0.0, 0.5, 2.0));
   Triangle tri2 = new Triangle.points(new Vector3(1.0, 0.0, 0.0),  new Vector3(1.0, 1.0, 0.0), new Vector3(2.0, 0.5, 2.0));
-  expect(Triangle_intersectsWithTriangle(tri1, tri2), false);
+  expect(Triangle_intersectsWithTriangleBasic(tri1, tri2), false);
 }
 
 void trianglesIntersectionCase1() {
@@ -23,14 +23,14 @@ void trianglesIntersectionCase1() {
 }
 
 void trianglesIntersectionCase2() {
-  Triangle tri1 = new Triangle.points(new Vector3(-1.0, 0.0, 0.0), new Vector3(0.0, 0.0, 2.0), new Vector3(1.0, 0.0, 0.0));
-  Triangle tri2 = new Triangle.points(new Vector3(-4.0, -1.0, 1.0),  new Vector3(0.0, 2.0, 0.0), new Vector3(4.0, -1.0, 1.0));
+  Triangle tri1 = new Triangle.points(new Vector3(-1.5, 0.0, 0.0), new Vector3(0.0, 0.0, -1.25), new Vector3(1.5, 0.0, 0.0));
+  Triangle tri2 = new Triangle.points(new Vector3(-1.5, 1.0, -0.5),  new Vector3(0.0, -1.0, -0.5), new Vector3(1.5, 1.0, -0.5));
   expect(Triangle_intersectsWithTriangle(tri1, tri2), true);
 }
 
 void trianglesIntersectionCase3() {
-  Triangle tri1 = new Triangle.points(new Vector3(0.0, 0.0, 0.0), new Vector3(0.0, 0.0, 0.0), new Vector3(0.0, 0.0, 0.0));
-  Triangle tri2 = new Triangle.points(new Vector3(0.0, 0.0, 0.0),  new Vector3(0.0, 0.0, 0.0), new Vector3(0.0, 0.0, 0.0));
+  Triangle tri1 = new Triangle.points(new Vector3(-1.5, 0.0, 0.0), new Vector3(0.0, 0.0, -1.25), new Vector3(1.5, 0.0, 0.0));
+  Triangle tri2 = new Triangle.points(new Vector3(-0.5, 1.0, -0.5),  new Vector3(1.0, -1.0, -0.5), new Vector3(2.5, 1.0, -0.5));
   expect(Triangle_intersectsWithTriangle(tri1, tri2), true);
 }
 
