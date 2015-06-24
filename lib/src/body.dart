83772849507ad29f2a1d6527a275964838f22365
grizet_j <jean.grizet@epitech.eu>
2015-06-24 18:57:19 -0400

    INit body from triangles

--- lib/src/body.dart
+++ lib/src/body.dart
@@ -3,10 +3,25 @@ part of rapid;
 //Define a rigid body container class
 class Body
 {
-  var      _local_timer = 0.0;
-  Collider _bounds;
+  var           _local_timer = 0.0;
+  List<Vector3> _vertices;
+  List<int>     _faces;
+  Collider      _bounds;
 
-  Body();
+  Body.empty() {
+    _vertices = new List<Vector3>();
+    _faces = new List<int>();
+    _bounds = null;
+  }
+
+  //Creates a body from a list of [faces] and
+  //linked [vertices]
+  //TODO add Collider choice
+  Body.fromTriangles(List<Vector3> vertices, List<int> faces) {
+    _vertices == vertices;
+    _faces = faces;
+    _bounds = new ObbCollider.fromTriangles(_faces, _vertices);
+  }
 
   //Moves a body of [delta] in model space
   void translate(Vector3 delta) {
